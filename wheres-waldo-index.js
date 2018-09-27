36664c8ee39e6d7b48962524463aaafc96eea9b1
Jenny Poon <jenny.poon@live.ca>
2018-09-27 18:57:33 +0000

    refactor code to use forEach method instead of For Loop

--- wheres-waldo-index.js
+++ wheres-waldo-index.js
@@ -1,15 +1,24 @@
 var result = []; //store data from functions into global variables for access
 
+//for each statement
 function findWaldo(arr, found) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
+  arr.forEach(function(word, i) {
+    if (word === "Waldo") {
       result = i;
       found();
-    }
-  }
+      }
+  })
+};
 
-  console.log(result)
-}
+// for loop
+// function findWaldo(arr, found) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       result = i;
+//       found();
+//     }
+//   }
+// }
 
 function actionWhenFound() {
   console.log("Found Waldo at index " + result + "!");
